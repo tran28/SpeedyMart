46a3e4c66ce77fbe93a7c6dedd091dffd0034717
philipn7 <35081218+philipn7@users.noreply.github.com>
2022-04-19 20:13:12 -0400

    Revert "admin page"
    This reverts commit 01261d3037569b43df7d8b4b1364ce60dfdcc84b.

--- client/src/components/account/Admin.js
+++ client/src/components/account/Admin.js
@@ -196,7 +196,6 @@ function Admin() {
                             </div>
                         </div>
                     </div>
-                    ...
                 </div>
             </div>
         </>
